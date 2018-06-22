29c2a74d40b574ffdc0b125b62bd88307c385e0a
flaviomau <flaviomau@yahoo.com>
2018-06-22 08:49:27 -0300

    connect all objects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Counter from './Counter'
+import counterReducer from './reducer'
+
+let store = createStore(counterReducer)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Counter />
-      </div>
-    );
+      <Provider store={store}>
+        <div className="App">
+          <Counter />
+        </div>
+      </Provider>      
+    )
   }
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { increment, decrement } from './actions'
 
 class Counter extends Component{
     render(){
         return (
             <p>
-                Counter: 0
-                <button>+</button>
-                <button>-</button>
+                Counter: {this.props.count}
+                <button onClick={this.props.increment}>+</button>
+                <button onClick={this.props.decrement}>-</button>
             </p>
         )
     }
 }
 
-export default Counter
+const mapStateToProps = (state) => {
+    return {
+        count: state.count
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment),
+        decrement: () => dispatch(decrement) 
+    }
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )(Counter)
